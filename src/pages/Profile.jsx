6751c0a01e9ecac1abca3de6c8ed69258af20aeb
farukxx11 <farukxx1122@gmail.com>
2025-11-13 22:08:29 +0600

    Implement Update Transaction page and update logic

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,10 +4,8 @@ import { useAuth } from "../context/AuthContext";
 const Profile = () => {
   const { user, logout, loading } = useAuth();
 
-  // Show loading state while fetching user data
   if (loading) return <p className="text-center mt-10">Loading user info...</p>;
 
-  // If no user is logged in, display a message
   if (!user)
     return (
       <p className="text-center mt-10">Please log in to view your profile.</p>
@@ -18,7 +16,6 @@ const Profile = () => {
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md text-center">
         <h2 className="text-2xl font-bold mb-4">User Profile</h2>
 
-        {/* Profile Image */}
         <div className="mb-4">
           {user.photoURL ? (
             <img
@@ -35,11 +32,9 @@ const Profile = () => {
           )}
         </div>
 
-        {/* User Info */}
         <p className="text-lg font-medium">Name: {user.displayName || "N/A"}</p>
         <p className="text-lg font-medium">Email: {user.email || "N/A"}</p>
 
-        {/* Logout Button */}
         <button
           onClick={logout}
           className="mt-6 w-full bg-red-500 text-white py-2 rounded hover:bg-red-600"
